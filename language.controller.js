f235834363a8c25e7afd2f35017425735ac3f110
MATU_LEGUI <92628713+Matulegui201297@users.noreply.github.com>
2023-05-07 21:18:12 -0300

    Actualizacion de codigos - cambio de base de datos de mysql a mongo

--- language.controller.js
+++ language.controller.js
@@ -1,5 +1,10 @@
-const getLanguages = (req, res) => {
-    res.json("Hola - Mundo")
+import { getConnection } from "./../Database/database";
+
+const getLanguages = async (req, res) => {
+    const connection = await getConnection()
+    const result = await connection.query("SELECt id, name, programmers FROM languages") 
+    console.log(result)
+    res.json(result)
 };
 
 export const methods = {
